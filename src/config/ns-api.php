d118775e68a2e9a837834bd4350ec79923516266
Edo Freriks <edofre@gmail.com>
2017-01-11 23:11:16 +0100

    Rename key > password

--- src/config/ns-api.php
+++ src/config/ns-api.php
@@ -2,5 +2,5 @@
 
 return [
     'username' => env('NS_API_USERNAME', 'Add your own username here'),
-    'password' => env('NS_API_KEY', 'Add your own password here'),
+    'password' => env('NS_API_PASSWORD', 'Add your own password here'),
 ];
