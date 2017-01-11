e42942479a1129c5850d6de3b879af8ec2032a2b
Edo Freriks <edofre@gmail.com>
2017-01-11 23:19:38 +0100

    Basic constructor and makeRequest

--- src/NsApi.php
+++ src/NsApi.php
@@ -8,7 +8,7 @@ use GuzzleHttp\Client;
  * Class NsApi
  * @package Edofre\NsApi
  */
-class NsApi extends Client
+class NsApi
 {
     /** API URL */
     const API_URL = 'http://webservices.ns.nl/';
@@ -16,9 +16,37 @@ class NsApi extends Client
     const ENDPOINT_STATIONS = '/ns-api-stations-v2';
     /** @var Client */
     private $client;
-    /** @var   */
+    /** @var */
     private $username;
-    /** @var   */
+    /** @var */
     private $password;
 
+    /**
+     * NsApi constructor.
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        $this->client = new Client(array_merge([
+            'base_uri' => self::API_URL,
+        ], $config));
+
+        // Make sure we have a username and password
+        $this->username = config('ns-api.username');
+        $this->password = config('ns-api.password');
+    }
+    
+    /**
+     * @param $request
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    private function makeRequest($request)
+    {
+        return $this->client->get($request, [
+            'auth' => [
+                $this->username,
+                $this->password,
+            ],
+        ]);
+    }
 }
