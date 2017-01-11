096c100648efb3d3f76792f9b5d26e50d4a4e433
Edo Freriks <edofre@gmail.com>
2017-01-11 23:02:18 +0100

    Add class variables

--- src/NsApi.php
+++ src/NsApi.php
@@ -2,11 +2,23 @@
 
 namespace Edofre\NsApi;
 
+use GuzzleHttp\Client;
+
 /**
  * Class NsApi
  * @package Edofre\NsApi
  */
-class NsApi
+class NsApi extends Client
 {
+    /** API URL */
+    const API_URL = 'http://webservices.ns.nl/';
+    /** Endpoints */
+    const ENDPOINT_STATIONS = '/ns-api-stations-v2';
+    /** @var Client */
+    private $client;
+    /** @var   */
+    private $username;
+    /** @var   */
+    private $password;
 
 }
