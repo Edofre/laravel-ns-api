27395dd029277e05bb73dda2ce3abddb3bc2a267
Edo Freriks <edofre@gmail.com>
2017-01-29 22:33:51 +0100

    Add announcement model

---
+++ src/Responses/Announcement.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Edofre\NsApi\Responses;
+
+use SimpleXMLElement;
+
+/**
+ * Class Announcement
+ * @package Edofre\NsApi\Responses
+ */
+class Announcement
+{
+    /** @var  string */
+    public $id;
+    /** @var  string */
+    public $severity;
+    /** @var  TravelSection[] */
+    public $announcement;
+
+    /**
+     * Announcement constructor.
+     * @param $id
+     * @param $severity
+     * @param $announcement
+     */
+    function __construct($id, $severity, $announcement)
+    {
+        $this->id = $id;
+        $this->severoty = $severity;
+        $this->announcement = $announcement;
+    }
+
+    /**
+     * @param SimpleXMLElement $xml
+     * @return Announcement
+     */
+    public static function createFromXml(SimpleXMLElement $xml)
+    {
+        return new self(
+            (string)$xml->Id,
+            (string)$xml->Ernstig,
+            (string)$xml->Text
+        );
+    }
+}
